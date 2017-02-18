838b16a993f2571732dc2e329be9151b8ca17b61
Deyanira Mares <deyamares@knights.ucf.edu>
2017-02-18 15:02:05 -0500

    Create Questions.js

---
+++ src/Questions.js
@@ -0,0 +1,422 @@
+'use strict';
+module.exports = {
+
+ "QUESTIONS" : [
+  {
+      "What is the name of Corrin's side-b?": [
+        "Dragon Lunge",
+        "Dragon Fang",
+        "Blazer",
+        "Dragon Leap",
+        "Paralyzer Shot"
+        
+       ]
+   },
+   {
+      "What tier is Olimar? ": [
+        "E",
+        "F",
+        "D",
+        "C",
+        "B"
+      ]
+   },
+   {
+      "Pugwest mains who? ": [
+        "Marth",
+        "Lucina",
+        "Shulk",
+        "Ike",
+        "Roy"
+      ]
+    },
+    { 
+      "Which character CANNOT meteor smash?": [
+        "Jigglypuff",
+        "Wario",
+        "Wii Fit Trainer",
+        "Greninja",
+        "Dark Pit"
+       ]
+     },
+     {
+       "Which character is most likely to win vs a Wii Fit Trainer?": [
+        "Mr. Game and Watch",
+        "Ganondorf",
+        "Charizard",
+        "Bowser Jr.",
+        "Link"
+        ]
+      },
+      {
+        "What is the name of Lucario's recovery move?": [
+          "Extreme Speed",
+          "Extreme Jump",
+          "Extreme Velocity",
+          "Extreme Air",
+          "Power Overwhelming"
+          ]
+      },
+      {
+        "Pac-Man is what tier?": [
+         "F",
+         "C",
+         "H",
+         "G",
+         "B"
+        ]
+       },
+        {
+        "Peach does best vs which of the following characters?": [
+         "Jigglypuff",
+         "Cloud",
+         "Sheik",
+         "Olimar",
+         "Ness"
+        ]
+       },
+        {
+        "What tier is Rosalina and Luma places in?": [
+         "S",
+         "A",
+         "B",
+         "C",
+         "D"
+        ]
+       },
+        {
+        "What tier is Ryu placed in?": [
+         "A",
+         "S",
+         "B",
+         "C",
+         "D"
+        ]
+       },
+        {
+        "What tier is Yoshi placed in?": [
+         "D",
+         "A",
+         "C",
+         "B",
+         "G"
+        ]
+       },
+        {
+        "What tier is Luigi placed in?": [
+         "D",
+         "E",
+         "C",
+         "B",
+         "F"
+        ]
+       },
+        {
+        "What tier is Villager placed in?": [
+         "B",
+         "A",
+         "C",
+         "S",
+         "D"
+        ]
+       },
+        {
+        "What tier is Megaman placed in?": [
+         "B",
+         "A",
+         "C",
+         "D",
+         "S"
+        ]
+       },
+        {
+        "Kirby is placed in what tier?": [
+         "F",
+         "G",
+         "E",
+         "B",
+         "H"
+        ]
+       },
+        {
+        "Palutena is placed in which tier?": [
+         "F",
+         "G",
+         "E",
+         "H",
+         "B"
+        ]
+       },
+        {
+        "Corrin is in what tier?": [
+         "C",
+         "D",
+         "B",
+         "A",
+         "S"
+        ]
+       },
+        {
+        "This character is Roy's worst match up: ": [
+         "Bayonetta",
+         "Jigglypuff",
+         "Duck Hunt",
+         "Dr. Mario",
+         "ROB"
+        ]
+       },
+        {
+        "This character is King Dedede's best match up": [
+         "Jigglypuff",
+         "Cloud",
+         "Corrin",
+         "Marth",
+         "Ness"
+        ]
+       },
+        {
+        "This character is Megaman's worst match up": [
+         "Fox",
+         "Duck Hunt",
+         "Rosalina",
+         "Sheik",
+         "Dark Pit"
+        ]
+       },
+        {
+        "Which character does MKLeo play?": [
+         "Marth",
+         "Ike",
+         "Roy",
+         "Corrin",
+         "Lucina"
+        ]
+       },
+        {
+        "Abadango mains which character?": [
+         "Mewtwo",
+         "Lucario",
+         "Megaman",
+         "Jigglypuff",
+         "Diddy Kong"
+        ]
+       },
+        {
+        "Who is the number one player in the world?": [
+         "ZeRo",
+         "Mr-R",
+         "Nairo",
+         "Ally",
+         "Dabuz"
+        ]
+       },
+        {
+        "What is the name of Bayonetta's neutral B": [
+         "Bullet Climax",
+         "Pistol Shot",
+         "Charging Bullets",
+         "Bullet Hell",
+         "Witch Shots"
+        ]
+       },
+        {
+        "What is the name of Dark Pit's recovery?": [
+         "Power of Flight",
+         "Wings of Might",
+         "Guardian Orbitars",
+         "Silver Bow",
+         "Ascent"
+        ]
+       },
+        {
+        "What is the name of Duck Hunt's down B?": [
+         "Wild Gunman",
+         "Hunters",
+         "Cowboy Shooters",
+         "Trick Shots",
+         "Bullet Showdown"
+        ]
+       },
+        {
+        "What is the name of Kirby's side B?": [
+         "Hammer Flip",
+         "Final Cutter",
+         "Stone",
+         "Inhale",
+         "Hammer Slam"
+        ]
+       },
+        {
+        "Which player is known to play Corrin": [
+         "Ryuga",
+         "Esam",
+         "Ally",
+         "Nairo",
+         "Ogma"
+        ]
+       },
+        {
+        "Which character is not a projectile character?": [
+         "Captain Falcon",
+         "Corrin",
+         "Mario",
+         "Luigi",
+         "Villager"
+        ]
+       },
+        {
+        "Which is NOT a legal stage?": [
+         "Lylat",
+         "Battle Field",
+         "Final Destination",
+         "Town and City",
+         "Dream Land"
+        ]
+       },
+        {
+        "What is the name of Fox's side B": [
+         "Fox Illusion",
+         "Reflector",
+         "Fire Fox",
+         "Blaster",
+         "Fox Dash"
+        ]
+       },
+        {
+        "What is the name of Mr. Game and Watch's side B?": [
+         "Judge",
+         "RNG",
+         "Judgement",
+         "Fire",
+         "Chef"
+        ]
+       },
+        {
+        "What is the name of Robin's down B?": [
+         "Nosferatu",
+         "Elwind",
+         "Thunder",
+         "Arcthunder",
+         "Arcfire"
+        ]
+       },
+        {
+        "What is the name of Wario's neutral B?": [
+         "Chomp",
+         "Wario Waft",
+         "Wario Bike",
+         "Corkscrew",
+         "Munch Munch"
+        ]
+       },
+        {
+        "What is the name of Zero Suit Samus's up B?": [
+         "Boost Kick",
+         "Flip Jump",
+         "Paralyzer",
+         "Plasma Whip",
+         "Booster Jump"
+        ]
+       },
+       {
+        "What is the name of Little Mac's down B?": [
+         "Slip Counter",
+         "Rising Uppercut",
+         "Straight Lunge",
+         "Jolt Haymaker",
+         "Valient Stand"
+        ]
+       },
+       {
+        "Who is the heaviest character in the game?": [
+         "Bowser",
+         "Donkey Kong",
+         "King Dedede",
+         "Charizard",
+         "Bowser Jr."
+        ]
+       },
+       {
+        "How should you DI Mega Man's down throw at the 100 range?": [
+         "Up and away",
+         "Down and inward",
+         "Up",
+         "Down",
+         "Right"
+        ]
+       },
+       {
+        "Which throw leads into many of Palutena's true combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Captain Falcon's combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Bowser's combos?": [
+         "Up throw",
+         "Down throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Charizard's combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Lucas's combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Mario's combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Ness's combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       },
+       {
+        "Which throw leads into many of Zero Suit Samus's combos?": [
+         "Down throw",
+         "Up throw",
+         "Rightward throw",
+         "Leftward throw",
+         "None"
+        ]
+       }
+      ]
+     };
+       
